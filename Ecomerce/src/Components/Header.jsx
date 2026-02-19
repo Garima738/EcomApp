7e2bf55dfe092b10af7763d28eb21cceaec2e560
garima <garima@garimas-MacBook-Air.local>
2026-02-19 15:03:25 +0530

    first commit

---
+++ Ecomerce/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header({cartCount}){
+    return(
+        <div>
+            <h2>
+                React Ecom APP
+            </h2>
+            <p>Cart Item:{cartCount}</p>
+
+
+        </div>
+    );
+}
